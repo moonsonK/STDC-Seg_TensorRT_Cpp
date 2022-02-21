e730c3bc7f3203911322573846ace159c89af3ce
integrity <clark.mourning@integritycsinc.com>
2022-02-20 18:53:12 -0700

    Building w/ anticipated CMakeLists

---
+++ STDC-Seg_TensorRT.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+
+int main(int argc, char** argv)
+{
+	std::cout << "Hey clark" << std::endl;
+}
